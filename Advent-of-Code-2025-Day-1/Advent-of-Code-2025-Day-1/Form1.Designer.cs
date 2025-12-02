33ec22f56e44c9bba3f1c5e7e8075eeb91261a02
PowerSurge9905 <powersurge9905@gmail.com>
2025-12-02 01:21:14 -0500

    1.0.0
    Solved Day 1 Puzzle 1

--- Advent-of-Code-2025-Day-1/Advent-of-Code-2025-Day-1/Form1.Designer.cs
+++ Advent-of-Code-2025-Day-1/Advent-of-Code-2025-Day-1/Form1.Designer.cs
@@ -28,12 +28,57 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            txtZeroHits = new TextBox();
+            lblText1 = new Label();
+            lblText2 = new Label();
+            SuspendLayout();
+            // 
+            // txtZeroHits
+            // 
+            txtZeroHits.Location = new Point(124, 88);
+            txtZeroHits.Name = "txtZeroHits";
+            txtZeroHits.ReadOnly = true;
+            txtZeroHits.Size = new Size(157, 27);
+            txtZeroHits.TabIndex = 0;
+            txtZeroHits.TabStop = false;
+            txtZeroHits.TextAlign = HorizontalAlignment.Center;
+            // 
+            // lblText1
+            // 
+            lblText1.AutoSize = true;
+            lblText1.Location = new Point(124, 65);
+            lblText1.Name = "lblText1";
+            lblText1.Size = new Size(157, 20);
+            lblText1.TabIndex = 1;
+            lblText1.Text = "This Input Set Hit Zero";
+            // 
+            // lblText2
+            // 
+            lblText2.AutoSize = true;
+            lblText2.Location = new Point(178, 118);
+            lblText2.Name = "lblText2";
+            lblText2.Size = new Size(48, 20);
+            lblText2.TabIndex = 2;
+            lblText2.Text = "Times";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(405, 203);
+            Controls.Add(lblText2);
+            Controls.Add(lblText1);
+            Controls.Add(txtZeroHits);
+            Name = "Form1";
+            Text = "Advent of Code Day 1";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private TextBox txtZeroHits;
+        private Label lblText1;
+        private Label lblText2;
     }
 }
--- Advent-of-Code-2025-Day-1/Advent-of-Code-2025-Day-1/Form1.cs
+++ Advent-of-Code-2025-Day-1/Advent-of-Code-2025-Day-1/Form1.cs
@@ -1,10 +1,73 @@
+using System.ComponentModel.Design;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+
 namespace Advent_of_Code_2025_Day_1
 {
     public partial class Form1 : Form
     {
+        public static string Dir = @"..\..\..\Input\";
+        public static string Path = Dir + "input";
+        public int position = 50;
+        public int zeroHits = 0;
+        StreamReader inputIn;
         public Form1()
         {
             InitializeComponent();
+
+            try
+            {
+                inputIn = new(new FileStream(Path, FileMode.Open, FileAccess.Read));
+                while (inputIn.Peek() != -1)
+                {
+                    string line = inputIn.ReadLine() ?? "L0";
+                    Regex splitter = new Regex(@"([a-zA-Z]+)(\d+)");
+                    Match result = splitter.Match(line);
+
+                    string direction = result.Groups[1].Value;
+                    int amount = int.Parse(result.Groups[2].Value);
+
+                    if (direction == "L")
+                    {
+                        position -= amount;
+                        while (position < 0)
+                        {
+                            Underflow();
+                        }
+                    }
+
+                    if (direction == "R")
+                    {
+                        position += amount;
+                        while (position > 99)
+                        {
+                            Overflow();
+                        }
+                    }
+
+                    if (position == 0)
+                    {
+                        zeroHits++;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+            txtZeroHits.Text = zeroHits.ToString();
+        }
+
+        public void Overflow()
+        {
+            position -= 100;
+        }
+
+        public void Underflow()
+        {
+            position += 100;
         }
     }
 }
